d4fadee3706b762dd49249824d6bc2b39ae3b81d
AJBats <albatsford@gmail.com>
2018-08-27 22:02:15 -0400

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Flashcards
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.flashcardPane = new System.Windows.Forms.Panel();
+            this.mainMenu = new System.Windows.Forms.Panel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.flashcardPane.SuspendLayout();
+            this.mainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(230, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(781, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 574);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(381, 99);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(399, 574);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(381, 99);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(786, 574);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(380, 99);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // flashcardPane
+            // 
+            this.flashcardPane.Controls.Add(this.pictureBox1);
+            this.flashcardPane.Controls.Add(this.button1);
+            this.flashcardPane.Controls.Add(this.button2);
+            this.flashcardPane.Controls.Add(this.button3);
+            this.flashcardPane.Location = new System.Drawing.Point(1196, 12);
+            this.flashcardPane.Name = "flashcardPane";
+            this.flashcardPane.Size = new System.Drawing.Size(1178, 684);
+            this.flashcardPane.TabIndex = 4;
+            // 
+            // mainMenu
+            // 
+            this.mainMenu.Controls.Add(this.button4);
+            this.mainMenu.Location = new System.Drawing.Point(12, 12);
+            this.mainMenu.Name = "mainMenu";
+            this.mainMenu.Size = new System.Drawing.Size(1178, 684);
+            this.mainMenu.TabIndex = 5;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(507, 273);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(206, 102);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Load Lesson";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2387, 710);
+            this.Controls.Add(this.mainMenu);
+            this.Controls.Add(this.flashcardPane);
+            this.Name = "Form1";
+            this.Text = "Flashcards!";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.flashcardPane.ResumeLayout(false);
+            this.mainMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel flashcardPane;
+        private System.Windows.Forms.Panel mainMenu;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Flashcards
+{
+    public partial class Form1 : Form
+    {
+        public List<String> answerSet = new List<string>();
+        public String[] questionSet;
+        public int rightAnswer = 0;
+        public int previousQuestion = -1;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.flashcardPane.Location = new System.Drawing.Point(12, 12);
+            this.mainMenu.Visible = true;
+            this.flashcardPane.Visible = false;
+
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            this.Size = new System.Drawing.Size(820, 500);            
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(rightAnswer == 0)
+            {
+                // yes
+                SetupQuestion();
+            }
+            else
+            {
+                // no
+                this.button1.ForeColor = Color.Red;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (rightAnswer == 1)
+            {
+                // yes
+                SetupQuestion();
+            }
+            else
+            {
+                // no
+                this.button2.ForeColor = Color.Red;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (rightAnswer == 2)
+            {
+                // yes
+                SetupQuestion();
+            }
+            else
+            {
+                // no
+                this.button3.ForeColor = Color.Red;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+                
+                string lineRead = sr.ReadLine();
+                while(lineRead != null)
+                {
+                    answerSet.Add(lineRead);
+                    lineRead = sr.ReadLine();
+                }
+
+                sr.Close();
+
+                string dirName = Path.GetDirectoryName(openFileDialog1.FileName);
+
+                String[] inputQuestionSet = Directory.GetFiles(dirName, "*.jpg", SearchOption.TopDirectoryOnly);
+                questionSet = new String[inputQuestionSet.Length];
+
+                foreach (String file in inputQuestionSet)
+                {
+                    int parsedResult;
+                    string pureFileName = Path.GetFileNameWithoutExtension(Path.GetFileName(file));                    
+
+                    if (int.TryParse(pureFileName, out parsedResult))
+                    {
+                        if(parsedResult == 0)
+                        {
+                            throw new System.Exception("Critical error: Do not use 0.jpg. Start questions at 1.jpg and count up.");
+                        }
+
+                        questionSet[parsedResult - 1] = file;
+                    }
+                    else
+                    {
+                        throw new System.Exception("Critical error: One of the input jpg files is not named after an integer value.");
+                    }
+                    
+                }
+
+                if (answerSet.Count != questionSet.Length)
+                {
+                    MessageBox.Show("Number of answers does not match number of questions.\nPlease correct data.");
+                    return;
+                }
+
+                if(answerSet.Count < 3)
+                {
+                    MessageBox.Show("Must have more than 3 questions in lesson.");
+                    return;
+                }
+            
+                SetupQuestion();
+
+                mainMenu.Visible = false;
+                flashcardPane.Visible = true;
+            }
+        }
+
+        private void SetupQuestion()
+        {
+            Random rnd = new Random();
+            List<String> answerSetCopy = new List<String>();
+
+            foreach(String s in answerSet)
+            {
+                answerSetCopy.Add(s);
+            }
+
+            int randoQuestion = rnd.Next(0, questionSet.Length);
+            int plzDontInfinite = 0;
+            // dont repeat last question
+            while(randoQuestion == previousQuestion)
+            {
+                plzDontInfinite++;
+                randoQuestion = rnd.Next(0, questionSet.Length);
+                if(plzDontInfinite > 100000)
+                {
+                    throw new System.Exception("Infinite loop detected.");
+                }
+            }
+            rightAnswer = rnd.Next(0, 3);
+
+            pictureBox1.Image = Image.FromFile(questionSet[randoQuestion]);
+
+            this.button1.ForeColor = Color.Black;
+            this.button2.ForeColor = Color.Black;
+            this.button3.ForeColor = Color.Black;
+
+
+            switch (rightAnswer)
+            {
+                case 0:
+                    button1.Text = answerSet[randoQuestion];
+                    answerSetCopy.Remove(button1.Text);
+                    button2.Text = answerSetCopy[rnd.Next(0, answerSetCopy.Count)];
+                    answerSetCopy.Remove(button2.Text);
+                    button3.Text = answerSetCopy[rnd.Next(0, answerSetCopy.Count)];
+                    break;
+                case 1:
+                    button2.Text = answerSet[randoQuestion];
+                    answerSetCopy.Remove(button2.Text);
+                    button1.Text = answerSetCopy[rnd.Next(0, answerSetCopy.Count)];
+                    answerSetCopy.Remove(button1.Text);
+                    button3.Text = answerSetCopy[rnd.Next(0, answerSetCopy.Count)];
+                    break;
+                case 2:
+                    button3.Text = answerSet[randoQuestion];
+                    answerSetCopy.Remove(button3.Text);
+                    button1.Text = answerSetCopy[rnd.Next(0, answerSetCopy.Count)];
+                    answerSetCopy.Remove(button1.Text);
+                    button2.Text = answerSetCopy[rnd.Next(0, answerSetCopy.Count)];
+                    break;
+                default:
+                    throw new System.Exception("rightAnswer out of bounds.");
+                    
+            }
+
+            previousQuestion = randoQuestion;
+        }
+    }
+}
